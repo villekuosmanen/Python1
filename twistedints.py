34b98b052c7e8936e703a2a8f140c668fc33cadf
al255 <al255@st-andrews.ac.uk>
2018-03-01 11:15:09 +0000

    Added proper comments to twistedints and twistedintegers, created html documentation

--- twistedints.py
+++ twistedints.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+#
+# Twisted Ints
+
 class TwistedInt:
     """The twisted int class"""
     def __init__(self, value, n):
-
+        """Initialise"""
         if value < 0:
             raise ValueError('Negative values not allowed')
         if value > n:
@@ -12,9 +16,11 @@ class TwistedInt:
         self.n = n
 
     def __str__(self, other=None):
+        """Output format: <a:n> where a ∈ Zn"""
         return ("<" + str(self.value) + ":" + str(self.n) + ">")
 
     def __add__(self, other):
+        """Addition rules: a ⊕ b = (a + b) mod n"""
         if self.n == other.n:
             total_value = (self.value + other.value) % self.n
             return TwistedInt(total_value, self.n)
@@ -22,6 +28,7 @@ class TwistedInt:
             raise ValueError('TwistedInts must have same n')
 
     def __mul__(self, other):
+        """Multiplication rules: a ⊗ b = (a + b + a · b) mod n"""
         if self.n == other.n:
             total_value = (self.value + other.value + self.value * other.value) % self.n
             return TwistedInt(total_value, self.n)
